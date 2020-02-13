c59b9eeaad2d3e6e5be36cd1a109c07a3a486153
Cristóbal Martínez <wipeoman@gmail.com>
2020-02-13 20:16:17 +0100

    Initial commit

---
+++ src/principal/navegador.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class navegador {
+
+	public static void main(String[] args) {
+		// Nombre base de datos: aadfinal
+		// Usuario: root
+		// Contraseña: rootroot
+	}
+
+}
