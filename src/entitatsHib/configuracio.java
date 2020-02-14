a1f6cba52988ee7ebcabff152087ee9ddd6dfe8b
Laura <lmartinezg@cifpfbmoll.eu>
2020-02-14 18:20:01 +0100

    configuracion Hib

--- src/entitatsHib/configuracio.java
+++ src/entitatsHib/configuracio.java
@@ -1,5 +1,27 @@
 package entitatsHib;
 
-public class configuracio {
+import java.io.File;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class configuracio {
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory == null) {
+			try {
+				Configuration configuration = new Configuration().configure(new File("hibernate.cfg.xml"));
+				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+						.applySettings(configuration.getProperties()).build();
+				sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return sessionFactory;
+	}
 }
