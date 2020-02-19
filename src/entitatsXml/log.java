56f67076beda6e697726b59d3fc05a36f35376ec
Cristóbal Martínez <wipeoman@gmail.com>
2020-02-19 16:09:28 +0100

    Added XML log & usuari

--- src/entitatsXml/log.java
+++ src/entitatsXml/log.java
@@ -1,5 +1,46 @@
 package entitatsXml;
 
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "log")
 public class log {
+	public int _id;
+	public String _text;
+	public Date _date;
+
+	public log() {
+		super();
+	}
+
+	@XmlAttribute(name = "id")
+	public int getId() {
+		return _id;
+	}
+
+	public void setId(int id) {
+		this._id = id;
+	}
+
+	@XmlElement(name = "text")
+	public String getText() {
+		return _text;
+	}
+
+	public void setText(String text) {
+		this._text = text;
+	}
+
+	@XmlElement(name = "date")
+	public Date getDate() {
+		return _date;
+	}
+
+	public void setDate(Date date) {
+		this._date = date;
+	}
 
 }
--- src/entitatsXml/usuari.java
+++ src/entitatsXml/usuari.java
@@ -1,5 +1,44 @@
 package entitatsXml;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "usuari")
 public class usuari {
+	public int _id;
+	public String _name;
+	public String _pass;
+
+	public usuari() {
+		super();
+	}
+
+	@XmlAttribute(name = "id")
+	public int getId() {
+		return _id;
+	}
+
+	public void setId(int id) {
+		this._id = id;
+	}
+
+	@XmlElement(name = "name")
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		this._name = name;
+	}
+
+	@XmlElement(name = "pass")
+	public String getPass() {
+		return _pass;
+	}
+
+	public void setPass(String pass) {
+		this._pass = pass;
+	}
 
 }
