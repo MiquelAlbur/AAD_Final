c9164adf1c9ef628c7f3ca1857fca0371ffa42d4
Miquel Alburquerque Marín <miquelalbur@gmail.com>
2020-02-23 19:17:47 +0100

    jeje

--- src/principal/mainUtils.java
+++ src/principal/mainUtils.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.dom.DOMSource;
@@ -18,43 +19,58 @@ import org.w3c.dom.Text;
 
 public class mainUtils {
 
-	public void DOMecho(String nomFitxer) {//Métode que s'encarrega d'utilitzar DOM per a llegir l'xml i guardarlo dins classes. Pot ser es podria fer estàtic?
-		
-		File fitxerXml = new File(nomFitxer + ".xml"); 
-		File dirActual = new File(""); 
+	public void DOMecho(String nomFitxer) {// Métode que s'encarrega d'utilitzar DOM per a llegir l'xml i guardarlo dins
+											// classes. Pot ser es podria fer estàtic?
+
+		File fitxerXml = new File(nomFitxer + ".xml");
+		File dirActual = new File("");
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder builder = factory.newDocumentBuilder();
-		DOMImplementation implementation = builder.getDOMImplementation();
-		Document document = implementation.createDocument(null, "dir", null);
-		document.setXmlVersion("1.0");
-		Element arrel = document.getDocumentElement();
-		arrel.setAttribute("name", dirActual.getAbsoluteFile().getName()); 
-		
-		// Generate XML 
-		//Element node;
-		Source source = new DOMSource(document); //Indicam on el volem guardar. 
-		Result result = new StreamResult(new java.io.File(fitxerXml.getAbsoluteFile().getName())); // nom de l'arxiu 
-		
-		//Métode separat al codi del profe
-		//implementar recorregut
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			DOMImplementation implementation = builder.getDOMImplementation();
+			Document document = implementation.createDocument(null, "dir", null);
+			document.setXmlVersion("1.0");
+			Element arrel = document.getDocumentElement();
+			arrel.setAttribute("name", dirActual.getAbsoluteFile().getName());
+			generatexml(document, fitxerXml);
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	private void generatexml(Document document, File fitxerXml) {
+		// Generate XML
+		// Element node;
+		Source source = new DOMSource(document); // Indicam on el volem guardar.
+		Result result = new StreamResult(new java.io.File(fitxerXml.getAbsoluteFile().getName())); // nom de l'arxiu
+	}
+
+	private void crearnodes(File dirActual, Document document /* x node, z formatDiaHora*/) {
+
+		// Métode separat al codi del profe
+		// implementar recorregut
 		File element;
-		File[] elementsDir = dirActual.getAbsoluteFile().listFiles(); 
+		File[] elementsDir = dirActual.getAbsoluteFile().listFiles();
 		Element fitxersNode = document.createElement("files");
 		node.appendChild(fitxersNode);
 		Element dirsNode = document.createElement("dirs");
-		node.appendChild(dirsNode); 
-		Element fitxerNode = document.createElement("file"); 
+		node.appendChild(dirsNode);
+		Element fitxerNode = document.createElement("file");
 		fitxerNode.setAttribute("name", element.getAbsoluteFile().getName());
 		Element fitxerData = document.createElement("date");
 		Date dateModificacio = new Date(element.getAbsoluteFile().lastModified());
 		Text nodeDateValue = document.createTextNode(formatDiaHora.format(dateModificacio));
-		fitxerData.appendChild(nodeDateValue); 
+		fitxerData.appendChild(nodeDateValue);
 		fitxerData.setNodeValue(formatDiaHora.format(dateModificacio));
 		fitxerNode.appendChild(fitxerData);
 		Element fitxerHidden = document.createElement("hidden");
 		Text nodeHiddenValue = document.createTextNode(String.valueOf(element.getAbsoluteFile().isHidden()));
 		fitxerHidden.appendChild(nodeHiddenValue);
 		fitxerNode.appendChild(fitxerHidden);
-		fitxersNode.appendChild(fitxerNode); 
+		fitxersNode.appendChild(fitxerNode);
+
 	}
 }
